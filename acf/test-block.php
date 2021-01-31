a4300e8df3649f4dbc9020a2418740caba08ae7e
Jamie Warburton <jamie@hexdigital.com>
2021-01-31 10:36:46 +0000

    refactor: shorten arrow function

--- acf/test-block.php
+++ acf/test-block.php
@@ -1,1 +1,1 @@
-<section x-data="{query: ''}" x-init="$watch('query', (value) => { console.log(this) } )"></section>
+<section x-data="{query: ''}" x-init="$watch('query', (value) => console.log(this) )"></section>
